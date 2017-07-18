2afb047fb1064fdbaeff533b9f642a9080ded130
Paul Roberts <pmr@stelo.org.uk>
2017-07-18 18:27:35 +0100

    bugfix

--- src/main/scala/smsqmulator/android/ScreenView.scala
+++ src/main/scala/smsqmulator/android/ScreenView.scala
@@ -14,14 +14,12 @@ object QLColours {
   val greenMask = flashMask << 1
 
   def memToPixel(data: Int, num: Int): (Int, Int, Int) = {
-    assert(num < 4 && num >= 0)
-    println((redMask << (num * 2)).toBinaryString)
     (if((data & (redMask << (num * 2))) == 0) 0 else 255,
       if((data & (greenMask << (num * 2))) == 0) 0 else 255,
       if((data & (blueMask << (num * 2))) == 0) 0 else 255)
   }
 
-  def memToPixels(data: Int): Seq[Int] = (0 to 3) map { n =>
+  def memToPixels(data: Int): Seq[Int] = (0 to 3).reverse map { n =>
     val (r, g, b) = memToPixel(data, n)
     Color.rgb(r, g, b)
   }
@@ -43,7 +41,6 @@ class ScreenView(context: Context, attrs: AttributeSet) extends SurfaceView(cont
       updateScreen
     }
     def surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) = {
-      Log.d(TAG, "surfaceChanged")
       updateScreen
     }
     def surfaceDestroyed(holder: SurfaceHolder) = {
@@ -69,12 +66,10 @@ class ScreenView(context: Context, attrs: AttributeSet) extends SurfaceView(cont
       val data = cpu.readMemoryWord(addr)
       QLColours.memToPixels(data)
     }
-    Log.d(TAG, s"Pixel length: ${pixels.size}")
     bitmap.setPixels(pixels.toArray, 0, 256, 0, 0, 256, 256)
   }
 
   protected def updateScreen = withCanvas { c =>
-    Log.d(TAG, s"ch: ${c.getHeight}, cw: ${c.getWidth}")
     updateBitmap
     c.drawBitmap(Bitmap.createScaledBitmap(bitmap, c.getWidth, c.getHeight, false), getMatrix, null)
     //c drawText ("hello world", 10, 10, QLColours.red)
