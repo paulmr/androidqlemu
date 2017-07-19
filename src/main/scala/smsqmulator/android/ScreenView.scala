bbbb7b92a2b93a3c3533866bbcea72e7282b162e
Paul Roberts <pmr@stelo.org.uk>
2017-07-19 11:35:09 +0100

    big speed up on the graphics drawing
    thanks to looking at smsqmulator

--- src/main/scala/smsqmulator/android/ScreenView.scala
+++ src/main/scala/smsqmulator/android/ScreenView.scala
@@ -7,32 +7,21 @@ import android.view.{ SurfaceView, SurfaceHolder }
 
 import android.graphics.{ Paint, Canvas, Bitmap, Color }
 
-object QLColours {
-  val blueMask = 1
-  val redMask  = blueMask << 1
-  val flashMask = blueMask << 8
-  val greenMask = flashMask << 1
-
-  def memToPixel(data: Int, num: Int): (Int, Int, Int) = {
-    (if((data & (redMask << (num * 2))) == 0) 0 else 255,
-      if((data & (greenMask << (num * 2))) == 0) 0 else 255,
-      if((data & (blueMask << (num * 2))) == 0) 0 else 255)
-  }
-
-  def memToPixels(data: Int): Seq[Int] = (0 to 3).reverse map { n =>
-    val (r, g, b) = memToPixel(data, n)
-    Color.rgb(r, g, b)
-  }
-}
-
 class ScreenView(context: Context, attrs: AttributeSet) extends SurfaceView(context, attrs) {
 
-  private val cpu = context.getApplicationContext.asInstanceOf[QDOSApplication].qdosMonitor.cpu
+  private val mon = context.getApplicationContext.asInstanceOf[QDOSApplication].qdosMonitor
+  private val cpu = mon.cpu
 
   private val TAG = "QLScreenView"
 
+  // mode 4
+  private val gWidth = 256
+  private val gHeight = 256
+
+  private var pixels = new Array[Int](gWidth * gHeight)
+
   // this may need to be modified if the screen mode changes etc, although I think we might be able to call Config
-  private lazy val bitmap = Bitmap.createBitmap(256, 256, Bitmap.Config.RGB_565)
+  private lazy val bitmap = Bitmap.createBitmap(gWidth, gHeight, Bitmap.Config.RGB_565)
 
   val holder = getHolder
 
@@ -58,15 +47,48 @@ class ScreenView(context: Context, attrs: AttributeSet) extends SurfaceView(cont
     }
   }
 
-  protected def updateBitmap = {
-    val screenBase = 0x20000
+  val pixelmask = Vector(0x203,0x80c,0x2030,0x80c0)
 
-    val pixels = ((0 until 0x8000) by 2) flatMap { count =>
-      val addr = screenBase + count
-      val data = cpu.readMemoryWord(addr)
-      QLColours.memToPixels(data)
+  protected def updateBitmap = {
+    val startTime = System.nanoTime
+    var addr = 0x20000
+    var pixelNum = 0
+
+    // this logic is nabbed from smsqmulator (thanks!)
+    while(addr < 0x28000) {
+      val data = mon.ram.readWord(addr)
+      for (i <- 0 to 3) {
+        val colourData = (data & pixelmask(i))>>>(i*2)
+        val colour = colourData match {
+          case 0 =>
+            Color.BLACK
+          case 1 =>
+            Color.BLUE
+          case 2 =>
+            Color.RED
+          case 3 =>
+            Color.MAGENTA
+          case 0x200 =>
+            Color.GREEN
+          case 0x201 =>
+            Color.CYAN
+          case 0x202 =>
+            Color.YELLOW
+          case 0x203 =>
+            Color.WHITE
+          case _ =>
+            Log.e(TAG, f"Unkown colour: $colourData%x (defaulting)")
+            Color.WHITE // ? was ORANGE
+        }
+        pixels(pixelNum + (3 - i)) = colour
+      }
+
+      pixelNum += 4
+      addr += 2
     }
-    bitmap.setPixels(pixels.toArray, 0, 256, 0, 0, 256, 256)
+    bitmap.setPixels(pixels, 0, gWidth, 0, 0, gWidth, gHeight)
+    val endTime = System.nanoTime
+    Log.d(TAG, s"took ${(endTime - startTime) / 1000000} ms")
   }
 
   protected def updateScreen = withCanvas { c =>
