4b6009449154e79579ec1b397d7b6a0e2dfc1b05
Paul Roberts <pmr@stelo.org.uk>
2017-08-03 09:20:00 +0100

    loadrom_android

--- android/src/main/scala/smsqmulator/android/QDOSApplication.scala
+++ android/src/main/scala/smsqmulator/android/QDOSApplication.scala
@@ -16,6 +16,18 @@ class QDOSApplication extends Application {
     Log.d(TAG, s)
   }
 
+  lazy val romFile: InputStream = {
+    try {
+      val fname = new File(getExternalFilesDir(null), "rom")
+      val res = new FileInputStream(fname)
+      log(s"using local rom ${fname.toString}")
+      res
+    } catch {
+      case e: IOException =>
+        log(s"local qlemu.rom not found, using packaged (${e.toString})")
+        getResources.openRawResource(TR.raw.rom.resid)
+    }
+  }
   // lazy val promFile: Option[InputStream] = {
   //   try {
   //     val fname = new File(getExternalFilesDir(null), "qlemu.rom")
@@ -30,8 +42,5 @@ class QDOSApplication extends Application {
   // }
 
   lazy val qdosMonitor =
-    new qdos.QDOSMonitor(
-      romFile = getResources.openRawResource(TR.raw.rom.resid),
-      promFile = None
-    )
+    new qdos.QDOSMonitor( romFile = romFile, promFile = None)
 }
--- build.sbt
+++ build.sbt
@@ -2,13 +2,11 @@ scalaVersion in ThisBuild := "2.11.8"
 
 val common = (project in file("common"))
   .disablePlugins(AndroidApp)
-  .settings(exportJars := true,
-    libraryDependencies += "com.typesafe" % "config" % "1.3.1")
+  .settings(exportJars := true)
   //.settings(BuildRom.settings:_*)
 
 val console = (project in file("console"))
   .disablePlugins(AndroidApp)
-  .settings(libraryDependencies += "org.jline" % "jline" % "3.3.1")
   .dependsOn(common)
 
 val android = (project in file("android"))
